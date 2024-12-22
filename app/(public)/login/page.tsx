1bfdbe6f1563a913820097a15adf1698530b83b4
jm <josemateus.ufma@gmail.com>
2024-12-22 10:12:14 -0300

    feat: login page done

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -9,10 +9,40 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { PersonStanding } from 'lucide-react';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Email inválido' }),
+  password: z.string().min(6, { message: 'Senha com tamanho inválido' }),
+});
 
 export default function LoginPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema), // liga zod ao react-hook-form
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const submit = () => {
+    console.log(form.getValues());
+  };
+
   return (
     <>
       <PersonStanding size={50} className="text-primary" />
@@ -21,7 +51,42 @@ export default function LoginPage() {
           <CardTitle>Entrar</CardTitle>
           <CardDescription>Faça o seu login aqui</CardDescription>
         </CardHeader>
-        <CardContent>login form</CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(submit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="mb-4">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="example@provider.com" {...field} />
+                    </FormControl>
+                    {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="mb-4">
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="****" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
+        </CardContent>
         <CardFooter className="flex justify-between">
           <small>Não tem conta?</small>
           <Button asChild variant={'outline'} size={'sm'}>
