44d2151893aadfa0cd3d862965339bef7761574b
jm <josemateus.ufma@gmail.com>
2025-01-26 10:57:51 -0300

    sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,45 @@
-import { ReactNode } from 'react';
-import MainMenu from './components/main-menu';
-import TopBar from './components/top-bar';
+import { AppSidebar } from '@/components/custom/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+export default function Page({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-[300px_1fr] h-100">
-      <MainMenu />
-      <div className="flex flex-col w-full">
-        <div>
-          {' '}
-          <TopBar />{' '}
-        </div>
-        <div className="p-4 md:bg-muted">
-          <h1 className="pb-4">Bem vindo, teste!</h1>
-          {children}
-        </div>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
